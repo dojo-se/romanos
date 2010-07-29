5825b985c425d94e8baf1fcec2fb2226e4a5cea2
Coding Dojo Sergipe <dojo-se@googlegroups.com>
2010-07-29 20:27:50 -0300

    Passando no teste do mapeamento. Hugo e Brandini

--- romano/tests.py
+++ romano/tests.py
@@ -3,10 +3,17 @@ import unittest
 
 def roman2deciman(rom):
 
+    map = {'I':1,'V':5,'X':10, 'L':50, 'C':100,
+        'M':1000}
+
+    if map.get(rom) == 10: return map[rom]
+
     if rom == 'IV':
         return 4
     elif rom == 'V':
         return 5
+    elif rom == 'VI':
+        return 6
     else:
         return len(rom)
 
@@ -21,6 +28,37 @@ class StubTests(unittest.TestCase):
 
     def testAllWords(self):
         self.assertEquals(roman2deciman('V'), 5)
+
+    def testWagner(self):
+        self.assertEquals(roman2deciman('VI'), 6)
+
+    def testMapeamento(self):
+        self.assertEquals(roman2deciman('X'), 10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
